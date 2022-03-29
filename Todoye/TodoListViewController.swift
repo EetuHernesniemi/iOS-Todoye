c24ff0f19c74eeec79a1deb571f6e53edb40f2b7
EetuHernesniemi <eetu.hernesniemi@hotmail.com>
2022-03-29 11:04:35 +0300

    table view with navigation controller

--- Todoye/TodoListViewController.swift
+++ Todoye/TodoListViewController.swift
@@ -7,13 +7,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TodoListViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 
 }
 
