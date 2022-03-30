c116c832f9cfe055bd78855977bd1d17b7456705
EetuHernesniemi <eetu.hernesniemi@hotmail.com>
2022-03-31 01:10:56 +0300

    save todoye list sadata to UserDefaults

--- Todoye/TodoListViewController.swift
+++ Todoye/TodoListViewController.swift
@@ -9,10 +9,14 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     var itemArray = ["Find Mike", "Buy Eggs", "Destroy Demogorgon."]
+    
+    let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = items
+        }
     }
     
     //MARK: - Tableview Datasource Methods
@@ -45,6 +49,7 @@ class TodoListViewController: UITableViewController {
         let alert = UIAlertController(title: "Add New Todoye Item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             self.itemArray.append(textField.text!)
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
             self.tableView.reloadData()
         }
         alert.addTextField { (alertTextField) in
