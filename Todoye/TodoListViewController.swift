5544b822093515c43d535d7aee88cf1294a81407
EetuHernesniemi <eetu.hernesniemi@hotmail.com>
2022-03-30 11:21:29 +0300

    deselect row effect and print

--- Todoye/TodoListViewController.swift
+++ Todoye/TodoListViewController.swift
@@ -26,5 +26,12 @@ class TodoListViewController: UITableViewController {
         cell.textLabel?.text = itemArray[indexPath.row]
         return cell
     }
+    
+    //MARK - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row])
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
 
