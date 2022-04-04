c4bfb17ee8bc43a97f7f1a8e2124059c022738ef
EetuHernesniemi <eetu.hernesniemi@hotmail.com>
2022-04-04 15:04:39 +0300

    Item category view draft.

--- Todoye/Controllers/TodoListViewController.swift
+++ Todoye/Controllers/TodoListViewController.swift
@@ -10,11 +10,16 @@ import CoreData
 
 class TodoListViewController: UITableViewController {
     var itemArray = [Item]()
+    var selectedCategory : ItemCategory? {
+        //Everything in didSet gets executed when selectedCategory is assigned a value.
+        didSet {
+            loadItems()
+        }
+    }
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadItems()
     }
     
     //MARK: - Tableview Datasource Methods
@@ -76,6 +81,7 @@ class TodoListViewController: UITableViewController {
             let newItem = Item(context: self.context)
             newItem.title = textField.text!
             newItem.done = false
+            newItem.parentCategory = self.selectedCategory
             
             self.itemArray.append(newItem)
             self.saveItems()
